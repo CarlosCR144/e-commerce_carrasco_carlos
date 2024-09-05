6d7814063cdb08ca02c2d34296ed48f13cfeb10b
carlos <carlos@gmail.com>
2024-09-05 18:59:11 -0400

    Modificar cards con JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("AAA");
+const h5 = document.querySelector("h5");
+const p = document.querySelector("p");
+const img = document.querySelector("img")
+
+h5.innerText = "ola";
+p.innerText = "XD"
+img.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgXtHworILI76AM15sMU8q2H3cAvdbdBdLtA&s");
