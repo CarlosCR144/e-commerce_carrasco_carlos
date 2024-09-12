d252fd723f0b9215bda79b392f1143bad779aeac
carlos <carlos@gmail.com>
2024-09-12 19:45:19 -0300

    Crear cards con JS

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const h5 = document.querySelector("h5");
-const p = document.querySelector("p");
-const img = document.querySelector("img")
+// Asignar los elementos HTML para modificar
+const h1 = document.querySelector("h1");
+const section = document.querySelector("section");
 
-h5.innerText = "ola";
-p.innerText = "XD"
-img.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgXtHworILI76AM15sMU8q2H3cAvdbdBdLtA&s");
+// Modificar texto de h1
+h1.innerText = "Productos"
+
+// Inicializar variable "array" al asignarle un array vacío
+let array = []
+
+// Bucle para insertar las Cards al array "array"
+for (let i = 1; i < 10; i++) {
+    array.push(`
+        <div class="card" style="width: 18rem;">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Auto ${i}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+`)
+}
+
+// Transformar el array a string
+// Reemplazar las "," con cadena vacía
+section.innerHTML = array.join().replaceAll(",", "");
+
+
+
