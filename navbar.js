d4645fbbc4611233d64a1042f1e72092743b2857
carlos <carlos@gmail.com>
2024-10-01 19:51:30 -0300

    Navbar.js added to every page

---
+++ navbar.js
@@ -0,0 +1,16 @@
+let botones = [
+    {texto: "Inicio", href: "index.html"},
+    {texto: "Productos", href: "producto.html"},
+    {texto: "Contacto", href: "#"}
+]
+
+const header = document.querySelector("header");
+
+let menu = []
+
+for (let boton of botones) {
+    menu.push(`<a class="nav-item" href=${boton.href}>${boton.texto}</a>`)
+}
+
+header.innerHTML = menu.join().replaceAll(",", "");
+
